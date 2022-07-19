d6a14e7a0d592e276f0cd9eea31f43a93dcf2ce0
Mihaela Macari <mikaela.malanici@gmail.com>
2022-07-19 15:44:34 +0300

    Solved Task #8 - array functions from Arrays Module

---
+++ Arrays/Task 8/script.js
@@ -0,0 +1,7 @@
+// -------------------- Task #8 - array functions --------------------------
+
+const colors = ["red", "green", "blue"];
+const checkColor = (arr, color) => {
+  return arr.indexOf(color);
+};
+console.log(checkColor(colors, "blue"));
