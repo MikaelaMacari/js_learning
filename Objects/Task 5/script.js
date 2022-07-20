bd76ab941c7a1d8fb8dd6bd4b9f3cd319a0bcf5c
Mihaela Macari <mikaela.malanici@gmail.com>
2022-07-20 13:10:44 +0300

    Solved Task #5 - object processing from Objects Module

---
+++ Objects/Task 5/script.js
@@ -0,0 +1,16 @@
+//------------------- Task #5 - object processing --------------
+
+const user = {
+  name: "Person",
+  age: 123,
+  salary: 1542.33,
+  contacts: {
+    phone: "112",
+    email: "email@domain.com",
+  },
+  address: `Moldova`,
+};
+user.contacts.phone = "+37312345678";
+delete user.address;
+
+console.log(user);
