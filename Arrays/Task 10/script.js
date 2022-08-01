062de1f00a6cc91f9de9ef8b349fcae202dadcb2
Mihaela Macari <mikaela.malanici@gmail.com>
2022-08-01 14:18:19 +0300

    created dev branch

--- Arrays/Task 10/script.js
+++
@@ -1,8 +0,0 @@
-// -------------------- Task #10 - array functions --------------------------
-
-const arr = [8, 7, 2, 2, 3, 4, 2, 5, 5, 6, 7, 8, 5, 9, 0, 1];
-const uniqueSorted = (arr) => {
-  const newArr = new Set(arr);
-  return [...newArr].sort((a, b) => a - b);
-};
-console.log(uniqueSorted(arr));
--- Arrays/Task 8/script.js
+++
@@ -1,7 +0,0 @@
-// -------------------- Task #8 - array functions --------------------------
-
-const colors = ["red", "green", "blue"];
-const checkColor = (arr, color) => {
-  return arr.indexOf(color);
-};
-console.log(checkColor(colors, "blue"));
