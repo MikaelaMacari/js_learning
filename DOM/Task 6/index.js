713377f5a9da72f5e86333d57b0e637b4dc6948e
Mihaela Macari <mikaela.malanici@gmail.com>
2022-08-02 16:34:06 +0300

    edited drawChessBoard function from Task 7, DOM

--- DOM/Task 6/index.js
+++ DOM/Task 6/index.js
@@ -1,19 +1,16 @@
 const container = document.getElementById("container");
 const rows = document.querySelectorAll("tr");
-const cells = document.querySelectorAll("td");
 
-rows.forEach((row, i) => {
-  cells.forEach((cell, j) => {
-    if ((i + j) % 2 === 0) {
-      cell.style.backgroundColor = "black";
-    } else {
-      cell.style.backgroundColor = "white";
-    }
+const drawChessBoard = () => {
+  rows.forEach((row, rowIdx) => {
+    const cells = row.querySelectorAll("td");
+    cells.forEach((cell, cellIdx) => {
+      if ((rowIdx + cellIdx) % 2 === 0) {
+        cell.style.backgroundColor = "black";
+      } else {
+        cell.style.backgroundColor = "white";
+      }
+    });
   });
-});
-
-// for (let i = 0; i < rows.length; i++) {
-//   for (let j = 0; j < cells.length; j++) {
-//     cells[i].style.backgroundColor += (i + j) % 2 === 0 ? "black" : "white";
-//   }
-// }
+};
+drawChessBoard();
