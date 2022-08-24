57c1d18f6a0b118bff5d04df67c1c77fc97dac90
Mihaela Macari <mikaela.malanici@gmail.com>
2022-08-24 20:11:27 +0300

    updated userRequest function

--- Promises/Task 3/index.js
+++ Promises/Task 3/index.js
@@ -2,12 +2,10 @@ const result = document.getElementById("result");
 
 async function userRequest() {
   result.textContent = "Waiting for responce...";
-  return await new Promise((resolve) => {
-    setTimeout(() => {
-      const response = serverResponse().then((textContent) => (result.textContent = textContent));
-      resolve(response);
-    }, 2000);
-  });
+  const response = await serverResponse();
+  setTimeout(() => {
+    result.textContent = response;
+  }, 2000);
 }
 function serverResponse() {
   return new Promise((resolve) => {
